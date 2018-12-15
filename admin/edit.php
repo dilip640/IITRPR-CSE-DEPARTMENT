ea8f71290e65665636b4bd6704fa462f02880264
Aman214 <2017csb1067@iitrpr.ac.in>
2018-12-15 17:01:42 +0530

    Merge branch 'master' of https://github.com/dilip640/IITRPR-CSE-DEPARTMENT

---
+++ admin/edit.php
@@ -0,0 +1,16 @@
+<?php print('{}');
+	require("../db.php");
+	$data = json_decode(file_get_contents('php://input'), true);
+	$query = mysqli_query($con,"SELECT * FROM cources WHERE id='".$data['row_id']."' ");
+	if(mysqli_num_rows($query)==0){
+		$sqli = "INSERT INTO cources (code, name, type, pre_req)
+				VALUES ('".$data['code']."','".$data['name']."','".$data['type']."','".$data['pre_req']."')";
+		mysqli_query($con,$sqli);
+	}
+	else{
+		$sqli = "UPDATE cources SET code='".$data['code']."',name='".$data['name']."',type='".$data['type']."',pre_req='".$data['pre_req']."' WHERE id='".$data['row_id']."' ";
+		mysqli_query($con, $sqli);
+	}
+	die;
+	
+?>
