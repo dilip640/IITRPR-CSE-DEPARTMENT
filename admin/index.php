1492241a52c1d8f9a7b604a3ca9603a86ab5ef39
Aman214 <2017csb1067@iitrpr.ac.in>
2018-12-15 22:09:12 +0530

    login

--- admin/index.php
+++ admin/index.php
@@ -159,6 +159,10 @@
 						
                     
                 </div>
+													<form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+    </form>
+			
             </nav>  
 
 	</div>
